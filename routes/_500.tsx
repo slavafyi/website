6ab4c0172169c622b8eae54bd372f731a2da24ac
Slava Maksimov <hello@slavamak.com>
2024-04-13 14:40:43 +0300

    feat: skip layout for 500 page

--- routes/_500.tsx
+++ routes/_500.tsx
@@ -1,4 +1,9 @@
 import { ServerCodePage } from "./_404.tsx";
+import { type RouteConfig } from "$fresh/server.ts";
+
+export const config: RouteConfig = {
+  skipInheritedLayouts: true,
+};
 
 export default function InternalServerError() {
   return ServerCodePage({
