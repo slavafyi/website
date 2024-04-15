2930c0e39b47aa540e97060b2ae39fa3e27a7c5c
Slava Maksimov <hello@slavamak.com>
2024-04-15 12:30:51 +0300

    feat: add projects page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,6 +8,7 @@ import * as $_app from "./routes/_app.tsx";
 import * as $_layout from "./routes/_layout.tsx";
 import * as $about from "./routes/about.tsx";
 import * as $index from "./routes/index.tsx";
+import * as $projects from "./routes/projects.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
 
@@ -19,6 +20,7 @@ const manifest = {
     "./routes/_layout.tsx": $_layout,
     "./routes/about.tsx": $about,
     "./routes/index.tsx": $index,
+    "./routes/projects.tsx": $projects,
   },
   islands: {},
   baseUrl: import.meta.url,
