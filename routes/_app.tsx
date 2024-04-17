88cde54b2a66c70557bd4c09e29248a06bc7cff7
Slava Maksimov <hello@slavamak.com>
2024-04-17 11:50:08 +0300

    feat: add cached fonts

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,6 +2,72 @@ import { type PageProps } from "$fresh/server.ts";
 import { asset } from "$fresh/runtime.ts";
 import site from "../data/site.json" with { type: "json" };
 
+const cssString = `
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    src: url(${asset('/fonts/open-sans-v40-latin-regular.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: italic;
+    font-weight: 400;
+    src: url(${asset('/fonts/open-sans-v40-latin-italic.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 500;
+    src: url(${asset('/fonts/open-sans-v40-latin-500.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: italic;
+    font-weight: 500;
+    src: url(${asset('/fonts/open-sans-v40-latin-500italic.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 600;
+    src: url(${asset('/fonts/open-sans-v40-latin-600.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: italic;
+    font-weight: 600;
+    src: url(${asset('/fonts/open-sans-v40-latin-600italic.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    src: url(${asset('/fonts/open-sans-v40-latin-700.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Open Sans';
+    font-style: italic;
+    font-weight: 700;
+    src: url(${asset('/fonts/open-sans-v40-latin-700italic.woff2')}) format('woff2');
+  }
+`
+
 export default function App({ Component }: PageProps) {
   return (
     <html lang="en">
@@ -14,6 +80,7 @@ export default function App({ Component }: PageProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <style dangerouslySetInnerHTML={{__html: cssString}} />
       </head>
 
       <body>
