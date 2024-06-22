64759674b76f98d36c366c89de0625136b286eb3
Slava Maksimov <hello@slavamak.com>
2024-06-22 22:12:32 +0300

    fix: issue where fonts did not cached

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,72 +2,6 @@ import { type PageProps } from "$fresh/server.ts";
 import { asset } from "$fresh/runtime.ts";
 import site from "../data/site.json" with { type: "json" };
 
-const cssString = `
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 400;
-    src: url(${asset('/fonts/open-sans-v40-latin-regular.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: italic;
-    font-weight: 400;
-    src: url(${asset('/fonts/open-sans-v40-latin-italic.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 500;
-    src: url(${asset('/fonts/open-sans-v40-latin-500.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: italic;
-    font-weight: 500;
-    src: url(${asset('/fonts/open-sans-v40-latin-500italic.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 600;
-    src: url(${asset('/fonts/open-sans-v40-latin-600.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: italic;
-    font-weight: 600;
-    src: url(${asset('/fonts/open-sans-v40-latin-600italic.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 700;
-    src: url(${asset('/fonts/open-sans-v40-latin-700.woff2')}) format('woff2');
-  }
-
-  @font-face {
-    font-display: swap;
-    font-family: 'Open Sans';
-    font-style: italic;
-    font-weight: 700;
-    src: url(${asset('/fonts/open-sans-v40-latin-700italic.woff2')}) format('woff2');
-  }
-`
-
 export default function App({ Component }: PageProps) {
   return (
     <html lang="en">
@@ -76,11 +10,52 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{site.title}</title>
         <link rel="stylesheet" href={asset("/styles.css")} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
-        <style dangerouslySetInnerHTML={{__html: cssString}} />
+        <link
+          rel="preload"
+          href={asset("/fonts/open-sans-v40-latin-regular.woff2")}
+          as="font"
+          type="font/woff2"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href={asset("/fonts/open-sans-v40-latin-500.woff2")}
+          as="font"
+          type="font/woff2"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href={asset("/fonts/open-sans-v40-latin-600.woff2")}
+          as="font"
+          type="font/woff2"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href={asset("/fonts/open-sans-v40-latin-700.woff2")}
+          as="font"
+          type="font/woff2"
+          crossOrigin="true"
+        />
       </head>
 
       <body>
