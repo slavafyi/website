8aa62431cc60a0f848407606a4b4fcc4b52cb8b5
Slava Maksimov <204412653+slavafyi@users.noreply.github.com>
2025-03-30 21:28:20 +0300

    feat!: migrate from fresh to astro

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import redirects from './data/redirects.json'
+import deno from '@deno/astro-adapter'
+
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  site: 'https://slava.fyi',
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  redirects: redirects.reduce((acc, curr) => {
+    acc[curr.source] = {
+      status: curr.status,
+      destination: curr.destination
+    }
+    return acc
+  }, {}),
+  output: 'server',
+  adapter: deno(),
+})
